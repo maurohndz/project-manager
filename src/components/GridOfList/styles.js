b4e81c004653f73f99236807473e3ddc0726aa6c
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-08-21 12:50:57 -0430

    scroll GridOfList

--- src/components/GridOfList/styles.js
+++ src/components/GridOfList/styles.js
@@ -2,4 +2,13 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  overflow-x: auto;
+  padding-bottom: 20px;
+  &::-webkit-scrollbar {
+    height: 8px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #ccc;
+    border-radius: 4px;
+  }
 `
